6859c2d1dfcefe91da6d048a0e6f24886b4981e3
bupakaishuitang <yaojia3000@126.com>
2024-05-04 13:29:36 +0800

    updated auth logic

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
-# app.py
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import LoginManager
 from user.config import Config
+from user.models import db, User
+from user.auth import auth_blueprint
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here___'
 app.config.from_object(Config)
 
-db = SQLAlchemy(app)
+db.init_app(app)
+migrate = Migrate(app, db)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 login_manager.login_view = 'auth.login'
 login_manager.login_message = 'Please log in to access this page.'
 login_manager.login_message_category = 'info'
-
-from user.models import User
-from user.auth import auth_blueprint
 
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
@@ -28,10 +30,13 @@ def load_user(user_id):
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/sign-in', methods=['GET', 'POST'])
+def signin():
     return render_template('login.html')
 
+@app.route('/sign-up', methods=['GET', 'POST'])
+def signup():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- user/auth.py
+++ user/auth.py
@@ -1,7 +1,6 @@
-# auth.py
 from flask import Blueprint, request, redirect, render_template, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
-from user.models  import User, db
+from .models import User, db
 from werkzeug.security import check_password_hash
 
 auth_blueprint = Blueprint('auth', __name__)
@@ -9,41 +8,38 @@ auth_blueprint = Blueprint('auth', __name__)
 @auth_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
+        return redirect(url_for('index'))
 
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        # 检查用户名是否已存在
         if User.query.filter_by(username=username).first():
             flash('Username already exists')
             return render_template('register.html')
 
-        # 创建新用户并保存到数据库
         new_user = User(username=username)
         new_user.set_password(password)
         db.session.add(new_user)
         db.session.commit()
 
-        # 登录用户并重定向
         login_user(new_user)
-        return redirect(url_for('main.index'))
+        return redirect(url_for('index'))
 
     return render_template('register.html')
 
 @auth_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
+        return redirect(url_for('index'))
 
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
         user = User.query.filter_by(username=username).first()
-        if user and check_password_hash(user.hashed_password, password):
+        if user and user.check_password(password):
             login_user(user, remember=True)
             next_page = request.args.get('next')
-            return redirect(next_page or url_for('main.index'))
+            return redirect(next_page or url_for('index'))
         else:
             flash('Invalid username or password')
     return render_template('login.html')
@@ -51,4 +47,4 @@ def login():
 @auth_blueprint.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('main.index'))
+    return redirect(url_for('index'))
