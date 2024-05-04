7ecebfc63bfb90c29a27aabed8f06e99f5f83f35
Creammilky <Cream_milk@bupt.edu.cn>
2024-05-04 13:47:17 +0800

    Implemented signin

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_login import LoginManager, current_user, login_required
 from user.config import Config
+from user.models import db, User
+from user.auth import auth_blueprint
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here___'
 app.config.from_object(Config)
 
-db = SQLAlchemy(app)
+db.init_app(app)
+migrate = Migrate(app, db)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 login_manager.login_view = 'auth.login'
 login_manager.login_message = 'Please log in to access this page.'
 login_manager.login_message_category = 'info'
-
-from user.models import User
-from user.auth import auth_blueprint
 
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
@@ -26,12 +28,22 @@ def load_user(user_id):
 
 @app.route('/')
 def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('index_log'))
     return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/user')
+@login_required
+def index_log():
+    return render_template('index_log.html')
+
+@app.route('/sign-in', methods=['GET', 'POST'])
+def signin():
     return render_template('login.html')
 
+@app.route('/sign-up', methods=['GET', 'POST'])
+def signup():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/user/config.py
+++ src/user/config.py
@@ -2,6 +2,5 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-very-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+mysqlconnector://username:password@host/dbname'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost/ipfs_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
